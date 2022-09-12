95580431a9e56c02af58f5f3429cbd0c747bed8d
MccGrey <chuxzzie01@yahoo.com>
2022-09-12 09:07:18 +0100

    set components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Service from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonial from "./components/testimonials/Testimonial";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    //REACT FRAGMENTS
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Service />
+      <Portfolio />
+      <Testimonial />
+      <Contact />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./nav.css";
+const nav = () => {
+  return <div>nav</div>;
+};
+
+export default nav;
