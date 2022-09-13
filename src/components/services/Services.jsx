a7673d45748e7976008a0d42d0b15fed39988509
MccGrey <chuxzzie01@yahoo.com>
2022-09-13 13:06:50 +0100

    services section

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,7 +1,119 @@
 import React from "react";
 import "./services.css";
+import { BsCheck } from "react-icons/bs";
 const services = () => {
-  return <section id="services">Services</section>;
+  return (
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+          </ul>
+        </article>
+        {/* ======================END OF WEB DEV. ===============================*/}
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+          </ul>
+        </article>
+        {/* ======================END OF UI/UX DESIGN ===============================*/}
+        <article className="service">
+          <div className="service__head">
+            <h3>Technical Writing (Documentation)</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+          </ul>
+          {/* ======================END OF TECHNICAL WRITING ===============================*/}
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default services;
