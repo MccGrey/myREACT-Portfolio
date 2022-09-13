1cd64b5c0ac59a9a56e80b2cb9f6a83d6466d1ec
MccGrey <chuxzzie01@yahoo.com>
2022-09-13 14:22:42 +0100

    testimonial section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,135 @@
 import React from "react";
 import "./portfolio.css";
+import IMG1 from "../../assets/img1.png";
+import IMG2 from "../../assets/img2.png";
+import IMG3 from "../../assets/img3.png";
+import IMG4 from "../../assets/img4.png";
+import IMG5 from "../../assets/img5.png";
+import IMG6 from "../../assets/img6.png";
 const portfolio = () => {
-  return <div>portfolio</div>;
+  return (
+    <section id="portfolio">
+      <h5>My Portfolio</h5>
+      <h2>portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+
+          <h3>title</h3>
+          <div className="portfolio__item-cta">
+            <a href="http://github.com/" className="btn" target="_blank">
+              GitHub
+            </a>
+            <a
+              href="http://github.com/"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG2} alt="" />
+          </div>
+
+          <h3>title</h3>
+          <div className="portfolio__item-cta">
+            <a href="http://github.com/" className="btn" target="_blank">
+              GitHub
+            </a>
+            <a
+              href="http://github.com/"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG3} alt="" />
+          </div>
+
+          <h3>title</h3>
+          <div className="portfolio__item-cta">
+            <a href="http://github.com/" className="btn" target="_blank">
+              GitHub
+            </a>
+            <a
+              href="http://github.com/"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG4} alt="" />
+          </div>
+
+          <h3>title</h3>
+          <div className="portfolio__item-cta">
+            <a href="http://github.com/" className="btn" target="_blank">
+              GitHub
+            </a>
+            <a
+              href="http://github.com/"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG5} alt="" />
+          </div>
+
+          <h3>title</h3>
+          <div className="portfolio__item-cta">
+            <a href="http://github.com/" className="btn" target="_blank">
+              GitHub
+            </a>
+            <a
+              href="http://github.com/"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG6} alt="" />
+          </div>
+
+          <h3>title</h3>
+          <div className="portfolio__item-cta">
+            <a href="http://github.com/" className="btn" target="_blank">
+              GitHub
+            </a>
+            <a
+              href="http://github.com/"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default portfolio;
--- src/components/testimonials/Testimonial.jsx
+++ src/components/testimonials/Testimonial.jsx
@@ -1,7 +1,70 @@
 import React from "react";
 import "./testimonial.css";
+import AVATR1 from "../../assets/create.jpg";
+import AVATR2 from "../../assets/olarej.jpg";
+import AVATR3 from "../../assets/poppins.jpg";
+import AVATR4 from "../../assets/pops.jpg";
 const testimonial = () => {
-  return <div>testimonial</div>;
+  return (
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVATR1} alt="" />
+          </div>
+          <h5 className="client__name">Kendrick Stars</h5>
+          <small className="client__review">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt
+            dolores numquam culpa delectus, modi error quia exercitationem
+            tempora odit minima, aliquam autem facere ratione asperiores
+            quibusdam, placeat magnam ipsam eligendi.
+          </small>
+        </article>
+
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVATR1} alt="" />
+          </div>
+          <h5 className="client__name">Kendrick Stars</h5>
+          <small className="client__review">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt
+            dolores numquam culpa delectus, modi error quia exercitationem
+            tempora odit minima, aliquam autem facere ratione asperiores
+            quibusdam, placeat magnam ipsam eligendi.
+          </small>
+        </article>
+
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVATR1} alt="" />
+          </div>
+          <h5 className="client__name">Kendrick Stars</h5>
+          <small className="client__review">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt
+            dolores numquam culpa delectus, modi error quia exercitationem
+            tempora odit minima, aliquam autem facere ratione asperiores
+            quibusdam, placeat magnam ipsam eligendi.
+          </small>
+        </article>
+
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVATR1} alt="" />
+          </div>
+          <h5 className="client__name">Kendrick Stars</h5>
+          <small className="client__review">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt
+            dolores numquam culpa delectus, modi error quia exercitationem
+            tempora odit minima, aliquam autem facere ratione asperiores
+            quibusdam, placeat magnam ipsam eligendi.
+          </small>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default testimonial;
