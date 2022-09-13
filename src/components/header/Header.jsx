28c59321d92869293d3c940980900bf518117e84
MccGrey <chuxzzie01@yahoo.com>
2022-09-13 04:31:51 +0100

    FIXME: responsive container for small devices

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./header.css";
 import CTA from "./CTA";
-import ME from "../../assets/ME.jpg";
+import ME from "../../assets/ME.png";
 import HeaderSocial from "./HeaderSocials";
 
 const header = () => {
@@ -9,7 +9,7 @@ const header = () => {
     <header>
       <div className="container header__container">
         <h5>Hello I'm</h5>
-        <h1>Aguonye cornelius</h1>
+        <h1>Aguonye Cornelius</h1>
         <h5 className="text-light">Frontend Developer</h5>
         <CTA />
         <HeaderSocial />
